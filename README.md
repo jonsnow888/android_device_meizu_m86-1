768bfcfbfdf0cb1cb4c70be9fe21149e7f72623d
loneyjs <1153563035@qq.com>
2018-02-19 10:06:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Meizu PRO5
+==========
+
+This branch is for building LineageOS 15.1 ROMS
+
---
+++ extract-files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VENDOR=meizu
+DEVICE=m86
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ `; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    #adb pull /system/$FILE $BASE/$FILE
+    cp /home/loneyjs/sdat2img/system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
